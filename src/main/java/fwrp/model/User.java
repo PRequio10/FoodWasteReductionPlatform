2c23f8bfb5b72ce3f32d11a18148f6816a849e8f
John Requio <requ0005@algonquinlive.com>
2024-07-11 13:55:00 -0400

    Added user class and database properties.

---
+++ src/main/java/fwrp/model/User.java
@@ -0,0 +1,17 @@
+package fwrp.model;
+
+/**
+ * This model represent the user of the application.
+ * 
+ * @author
+ */
+public class User {
+	
+	private String userName;
+	private String passWord;
+	private String email;
+	private int phone;
+	private String userType;
+	
+	
+}
