ad4e765ce573d081aacfac57acfb0fe30db8d6a2
John Requio <requ0005@algonquinlive.com>
2024-07-19 22:00:13 -0400

    Update DBConnection and RegisServlet, Added jakartaservlet jar files

--- src/main/java/fwrp/JUnit/UserDAOTest.java
+++ src/main/java/fwrp/JUnit/UserDAOTest.java
@@ -1,26 +1,21 @@
 package fwrp.JUnit;
 
+import static org.junit.Assume.assumeNotNull;
 import static org.junit.jupiter.api.Assertions.*;
 import java.sql.SQLException;
 import org.junit.jupiter.api.Test;
 import fwrp.dao.UserDAOImpl;
 import fwrp.model.User;
+import junit.framework.TestFailure;
 
 class UserDAOTest {
 
 	@Test
-	void test() throws SQLException {
-		User user1 = new User();
-		
-		user1.setUserName("User1");
-		user1.setPassWord("Pass");
-		user1.setEmail("Email@email.com");
-		user1.setPhone(123456789);
-		user1.setUserType("Consumer");
+	void daoConnectiontest() throws SQLException {
 		
 		UserDAOImpl userReg = new UserDAOImpl();
 		
-		userReg.insertUser(user1);
+		assertNotNull(userReg, "DB Connection not null");
 		
 	}
 
