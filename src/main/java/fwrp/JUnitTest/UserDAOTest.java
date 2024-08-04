8a3c9ab9dae3cf18011046288f0bdb382a93e0c8
John Requio <requ0005@algonquinlive.com>
2024-08-04 15:13:47 -0400

    update the sql file and UserDAOTest

--- src/main/java/fwrp/JUnitTest/UserDAOTest.java
+++ src/main/java/fwrp/JUnitTest/UserDAOTest.java
@@ -6,8 +6,6 @@ import java.sql.*;
 import org.junit.jupiter.api.Test;
 import fwrp.dao.UserDAOImpl;
 import fwrp.model.User;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 
 /**
@@ -17,6 +15,9 @@ import org.junit.jupiter.api.Test;
  */
 class UserDAOTest {
 	
+	User user = new User();
+	UserDAOImpl userData = new UserDAOImpl();
+	
 
 	/**
 	 * Unit test of the insertUser method.
@@ -25,18 +26,15 @@ class UserDAOTest {
 	@Test
 	void userInserttest() throws SQLException {
 		
-		User user = new User();
 		user.setUserName("testUName");
 		user.setPassWord("testPW");
 		user.setEmail("test@test.com");
 		user.setPhone(658974123);
 		user.setUserType("Consumer");
 		
-		UserDAOImpl insert = new UserDAOImpl();
-		
 		try {
-			insert.insertUser(user);
-			assertNotNull(insert);
+			userData.insertUser(user);
+			assertNotNull(userData);
 			System.out.println("Successfully inserted user data.");
 		} catch (SQLException e) {
 			System.out.println("Failed to insert user date" + e.getMessage());
@@ -50,53 +48,49 @@ class UserDAOTest {
 	@Test
 	void validateUserTest() throws SQLException {
 		
-		User user = new User();
 		user.setUserName("testUName");
 		user.setPassWord("testPW");
 		user.setEmail("test@test.com");
 		user.setPhone(658974123);
 		user.setUserType("Consumer");
 		
-		UserDAOImpl insert = new UserDAOImpl();
-		
 		try {
-			insert.insertUser(user);
-			assertNotNull(insert);
+			userData.insertUser(user);
+			assertNotNull(userData);
 			System.out.println("Successfully inserted user data.");
 		} catch (SQLException e) {
 			System.out.println("Failed to insert user date" + e.getMessage());
 		}
 	}
 	
-//	/**
-//	 * Unit test of the getUserCount method.
-//	 * @throws SQLException
-//	 */
-//    @Test
-//    void getUserCountTest() throws SQLException {
-//        try {
-//            UserDAOImpl count = new UserDAOImpl();
-//            count.getUserCount("testUName");
-//            assertNotNull(count);
-//            System.out.println("Successfully retrieved user count.");
-//        } catch (SQLException e) {
-//            System.out.println("Failed to retrieve user count: " + e.getMessage());
-//        }
-//    }
-//    
-//    /**
-//     * Unit test of the resetUserCount method.
-//     * @throws SQLException
-//     */
-//    @Test
-//    void resetUserCountTest() throws SQLException {
-//        try {
-//            userDAO.resetUserCount("testUName");
-//            int count = userDAO.getUserCount("testUName");
-//            assertEquals(0, count);
-//            System.out.println("Successfully reset user count.");
-//        } catch (SQLException e) {
-//            System.out.println("Failed to reset user count: " + e.getMessage());
-//        }
-//    }
+	/**
+	 * Unit test of the getUserCount method.
+	 * @throws SQLException
+	 */
+    @Test
+    void getUserCountTest() throws SQLException {
+        try {
+        	userData.getUserCount("testUName");
+            assertNotNull(userData);
+            System.out.println("Successfully retrieved user count.");
+        } catch (SQLException e) {
+            System.out.println("Failed to retrieve user count: " + e.getMessage());
+        }
+    }
+    
+    /**
+     * Unit test of the resetUserCount method.
+     * @throws SQLException
+     */
+    @Test
+    void resetUserCountTest() throws SQLException {
+        try {
+        	userData.resetUserCount("testUName");
+            int count = userData.getUserCount("testUName");
+            assertEquals(0, count);
+            System.out.println("Successfully reset user count.");
+        } catch (SQLException e) {
+            System.out.println("Failed to reset user count: " + e.getMessage());
+        }
+    }
 }
